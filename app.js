df33849befa7489a9bf2fdc1049b75bc6ac8678b
nachikethmoger <nachikethbhatkal@gmail.com>
2022-02-01 16:47:18 +0530

    phase 1 level

---
+++ app.js
@@ -0,0 +1,4 @@
+let loc = document.getElementById("location");
+let tempicon = document.getElementById("temp-icon");
+let tempvalue = document.getElementById("temp-value");
+let climate = document.getElementById("climate");
